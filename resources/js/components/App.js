b7e14ad73363bfedbaa9cdf42e4bac395da559bb
Vitor Bertti <berttivitor@gmail.com>
2020-05-18 15:19:44 -0300

    Added Reactjs

---
+++ resources/js/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+   return (
+       <div className="container">
+           <div className="row justify-content-center">
+               <div className="col-md-8">
+                   <div className="card">
+                       <div className="card-header">Event Registration</div>
+
+                       <div className="card-body">Events</div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   );
+}
+
+export default App;
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/app.css">
+        <title>Event Registration</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    </head>
+    <body>
+    <div id="root"></div>
+   <script src="js/app.js"></script>
+    </body>
+</html>
