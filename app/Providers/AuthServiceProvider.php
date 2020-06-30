dc40c24c1d57c87d6bad74a3e4919cd77c4c4460
Vitor Bertti <berttivitor@gmail.com>
2020-06-30 17:11:03 -0300

    Added Authorization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\User;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,17 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->app['auth']->viaRequest('api', function (Request $request) {
+
+            if (!$request->hasHeader('Authorization')) {
+                return null;
+            }
+            $authorizationHeader = $request->header('Authorization');
+            $token = str_replace('Bearer', '', $authorizationHeader);
+            $authData = JWT::decode($token, env('JWT_KEY'), ['HS256']);
+
+            // return new GenericUser(['email' => $authData]);
+            return User::where('email', $authData('email'))->first();
+        });
     }
 }
